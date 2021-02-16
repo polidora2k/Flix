3e99bafdf3c0bd8f2268e062d4f70ee11c9fad10
Ian Polidora <polidora2k@gmail.com>
2021-02-16 13:36:12 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Ian Polidora on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
